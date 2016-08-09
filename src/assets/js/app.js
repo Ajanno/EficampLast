304cd217652b39e6e58d120c869c10caae888fba
Ajanno <daniel.matyka@gmail.com>
2016-08-09 19:38:55 +0200

    jquerry is here

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,50 @@
-$(document).foundation();
+//$(document).foundation();
+function efiapi(pass){
+	$.ajax({
+	  type: "post",
+	  data: {
+	    login: "efi",
+	    password: pass
+	  },
+
+	  url: "https://efigence-camp.herokuapp.com/api/login",
+	  error: function(response) {
+	    console.log(response.responseText);
+	  },
+	  success: function(response) {
+	    console.log(response);
+	  }
+	});
+	}
+
+
+
+
+
+$(document).on("ready", function(){
+
+	// funckcja 1
+	$(".submit-button").on("click", function(event){
+		event.preventDefault();
+		var login = $(".login-name").val();
+		console.log(login);
+
+
+		if (login.length != 0) {
+			efiapi(login);
+		} else {
+			alert('wprowadz hasło');
+		}
+
+	});
+
+	//funkcja 2
+	$(".login-avatar").on("click", function() {
+		alert($(this).attr("src"));
+		
+	});
+
+	//funkcja 3
+
+
+});
