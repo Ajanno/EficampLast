35299647b0e5cf4f9f66c61c725c6ef6257e2252
Ajanno <daniel.matyka@gmail.com>
2016-08-14 01:10:42 +0200

    validacja pola i hovery

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,10 @@
-//$(document).foundation();
+
+var $passField = $(".login-name");
+var	$noPassBox = $(".pass-none");
+var $submitButton = $(".submit-button");
+
+
+
 function efiapi(pass){
 	$.ajax({
 	  type: "post",
@@ -10,31 +16,71 @@ function efiapi(pass){
 	  url: "https://efigence-camp.herokuapp.com/api/login",
 	  error: function(response) {
 	    console.log(response.responseText);
+	    $(".pass-allert").html("<p>Blędne hasło </p>");
+
+
 	  },
 	  success: function(response) {
 	    console.log(response);
+
 	  }
+
 	});
 	}
 
+function is$passFieldIsempty(){
+	console.log($passField.val().length!=0);
+	return $passField.val().length!=0;
+}
+function hideOrShow$noPassBox(){
+	if(is$passFieldIsempty()){
+		$noPassBox.hide("slow");
+		console.log("pole jest pelne");
+	} else {
+		$noPassBox.show("slow");
+		console.log("pole jest puste");
+	}
+} 
 
 
 
 
 $(document).on("ready", function(){
+	//Chowam boxy i blokuje przycisk
+	$noPassBox.hide();
+	// validacja pola
+	$passField.focus(function(){
+		hideOrShow$noPassBox();
+		console.log("funkcja focus");
+	}).keyup(function(){
+		hideOrShow$noPassBox();
+		console.log("funkcja key up");
+	})
+	.keydown(function(){
+		hideOrShow$noPassBox();
+		console.log("funkcja key down");
+	});
+	
 
-	// funckcja 1
-	$(".submit-button").on("click", function(event){
+
+
+	
+
+	$submitButton.on("click", function(event){
 		event.preventDefault();
-		var login = $(".login-name").val();
+		var login = $passField.val();
 		console.log(login);
 
-
-		if (login.length != 0) {
+		//validacja wartosci iputa
+		if (login.length!=0) {
 			efiapi(login);
 		} else {
 			alert('wprowadz hasło');
 		}
+
+
+
+
 
 	});
 
@@ -45,6 +91,7 @@ $(document).on("ready", function(){
 	});
 
 	//funkcja 3
+	
 
 
 });
