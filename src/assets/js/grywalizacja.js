9495c1c8117c9ec85dd26157aa4a7a82bff96506
Ajanno <daniel.matyka@gmail.com>
2016-09-14 10:51:23 +0200

    kolejne poprawki

--- src/assets/js/grywalizacja.js
+++ src/assets/js/grywalizacja.js
@@ -4,18 +4,27 @@ $(document).on("ready", function() {
         event.preventDefault();
         $(this).removeClass("non-active").siblings(".grywalizacja-button").addClass('non-active');
 
-        if ($(this).hasClass("not-done")) {
-            $(".account-achievments.done").hide();
-
-        } else {
-            $(".account-achievments").show();
-        }
+        if($(this).hasClass("not-done")){
+        $(".thumbnail-container .column").hide();
+        $(".thumbnail-container .column.thumbnail-checked").show();
+}else {
+    $(".thumbnail-container .column").show();
+}
+       
     });
     //funkcja checked
-    $(".account-achievments").children('.checked').toggle();
-    $(".account-achievments").on("click", function(){
-        $(this).children('.progres').toggle();
-        $(this).children('.checked').toggle();
+    // $(".account-achievments").children('.checked').toggle();
+    // $(".account-achievments").on("click", function(){
+    //     $(this).children('.progres').toggle();
+    //     $(this).children('.checked').toggle();
 
+    // });
+    $(".thumbnail-container .column").on("click", function(){
+        if($(this).hasClass("thumbnail-checked")){
+            $(this).removeClass("thumbnail-checked"); 
+        } else{
+        $(this).addClass("thumbnail-checked");
+     }
     });
+
 }); //Ready End
